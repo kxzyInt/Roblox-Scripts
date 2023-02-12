14d1b12e094a2cb8c2431d4ee7594eaeee051976
kxzyInt <113261037+kxzyInt@users.noreply.github.com>
2023-02-12 17:15:56 +0100

    Update Script Hub - Novus Hub.lua

--- Script Hub - Novus Hub.lua
+++ Script Hub - Novus Hub.lua
@@ -1,13 +1,14 @@
-local Games = {
-	[10821317529] = "https://raw.githubusercontent.com/kxzyInt/Roblox-Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
+local scripts = {
+    [10821317529] = "https://raw.githubusercontent.com/kxzyInt/Roblox-Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
 }
 
-if Games[game.PlaceId] then
-	loadstring(game:HttpGet(Games[game.PlaceId]))()
+placeId = game.PlaceId
+if scripts[placeId] ~= nil then
+    loadstring(game:HttpGet(scripts[placeId]))()
 else
-	game.StarterGui:SetCore("SendNotification", {
-	    Title = "Novus Hub Notification";
-	    Text = "Game not supported";
-	    Duration = 10;
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Novus Hub Notification";
+        Text = "Game not supported";
+        Duration = 10;
     })
 end
